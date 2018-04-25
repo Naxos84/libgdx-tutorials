a948bf84f91d9fdc39ff7b1366642f497344f488
MatthiasKolley <matthias.kolley@googlemail.com>
2018-04-25 23:02:48 +0200

    Added basic networking tutorial (unfinished)

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,14 +2,14 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.Networking;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "My Gdx Game";
-		config.width = 800;
-		config.height = 600;
-		new LwjglApplication(new MyGdxGame(), config);
-	}
+    public static void main(final String[] arg) {
+        final LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "My Gdx Game";
+        config.width = 800;
+        config.height = 600;
+        new LwjglApplication(new Networking(), config);
+    }
 }
